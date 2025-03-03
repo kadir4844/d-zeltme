140c2685d4e3e074ef16dceef95c5536c7aa8edf
Mehmetbeydiyceksiniz63 <149189769+Mehmetbeydiyceksiniz63@users.noreply.github.com>
2025-03-03 20:31:01 +0300

    Update start.py

--- ArchMusic/utils/inline/start.py
+++ ArchMusic/utils/inline/start.py
@@ -107,7 +107,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
             [
                 InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
                 InlineKeyboardButton(
-                    text=_["S_B_6"], url=f"https://github.com/ArchBots/ArchMusic"
+                    text=_["S_B_6"], url=f"https://t.me/the_team_kumsal"
                 ),
             ]
         )
@@ -116,7 +116,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
             buttons.append(
                 [
                     InlineKeyboardButton(
-                        text=_["S_B_6"], url=f"https://github.com/kumsalfed6301/Kumsal-muzik-bot"
+                        text=_["S_B_6"], url=f"https://t.me/the_team_kumsal"
                     ),
                 ]
             )
