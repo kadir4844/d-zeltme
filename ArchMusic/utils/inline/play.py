ae49f2cb878f7514fde373361085f0cb1d99286d
Mehmetbeydiyceksiniz63 <149189769+Mehmetbeydiyceksiniz63@users.noreply.github.com>
2025-03-03 19:54:11 +0300

    Update play.py

--- ArchMusic/utils/inline/play.py
+++ ArchMusic/utils/inline/play.py
@@ -2,28 +2,28 @@ import random
 
 from pyrogram.types import InlineKeyboardButton
 
-    if 0 < umm <= 10:
-        return "▰▱▱▱▱▱▱▱▱"
-    elif 10 < umm <= 20:
-        return "▰▰▱▱▱▱▱▱▱"
-    elif 20 < umm <= 30:
-        return "▰▰▰▱▱▱▱▱▱"
-    elif 30 < umm <= 40:
-        return "▰▰▰▰▱▱▱▱▱"
-    elif 40 < umm <= 50:
-        return "▰▰▰▰▰▱▱▱▱"
-    elif 50 < umm <= 60:
-        return "▰▰▰▰▰▰▱▱▱"
-    elif 60 < umm <= 70:
-        return "▰▰▰▰▰▰▰▱▱"
-    elif 70 < umm <= 80:
-        return "▰▰▰▰▰▰▰▰▱"
-    elif 80 < umm <= 90:
-        return "▰▰▰▰▰▰▰▰▰"
-    elif 90 < umm <= 100:
-        return "▰▰▰▰▰▰▰▰▰▰"
-    else:
-        return "▱▱▱▱▱▱▱▱▱"
+    
+         "▰▱▱▱▱▱▱▱▱"
+    
+         "▰▰▱▱▱▱▱▱▱"
+    
+         "▰▰▰▱▱▱▱▱▱"
+    
+         "▰▰▰▰▱▱▱▱▱"
+    
+         "▰▰▰▰▰▱▱▱▱"
+    
+         "▰▰▰▰▰▰▱▱▱"
+    
+         "▰▰▰▰▰▰▰▱▱"
+    
+         "▰▰▰▰▰▰▰▰▱"
+    
+         "▰▰▰▰▰▰▰▰▰"
+    
+         "▰▰▰▰▰▰▰▰▰▰"
+    
+         "▱▱▱▱▱▱▱▱▱"
 
 
 def stream_markup_timer(_, videoid, chat_id, played, dur):
